187b906109fbdc2f37ffe3f27c231438cdd3af07
Denisse Dominguez <A01702603@itesm.mx>
2022-02-27 16:50:18 -0600

    Funciones aplicadas

--- Lab8_Node/index.js
+++ Lab8_Node/index.js
@@ -1,6 +1,4 @@
 
-const filesystem = require('fs');
-
 //const http = require('http');
 
 //const server = http.createServer( (request, response) => {
@@ -10,7 +8,7 @@ const filesystem = require('fs');
 //server.listen(3000);
 
 
-// primera parte
+// recibir un arreglo y calcular su promedio
 function promedio(){
     const arreglo = [34, 75, 2, 45, 1, 75, 25, 91];
     let suma = 0;
@@ -21,7 +19,49 @@ function promedio(){
     }
 
     let resultado = suma / arreglo.length;
-    console.log("el promedio del arreglo [34,75,2,45,1,75,25,91] es " + resultado);
+    document.write("El promedio del arreglo [34,75,2,45,1,75,25,91] es = " + resultado);
 }
 
 promedio();
+
+// recibir una string y pasarla a texto
+const filesystem = require('fs');
+filesystem.writeFileSync('arreglo.txt', 'Probando la creacion de archivos de texto');
+
+// ordenar numeros
+const x= prompt("Introduce un número: ")
+const y= prompt("Introduce un número: ")
+const z= prompt("Introduce un número: ")
+
+if(x >= y && x >= z){ // x es el mayor
+    if(y >= z){
+        document.write(x);
+        document.write(y);
+        document.write(z);
+    }else{
+        document.write(x);
+        document.write(z);
+        document.write(y);
+    }
+}else if(y >= x && y >= z){ // y es el mayor
+    if ( x >= z){
+        document.write(y);
+        document.write(x);
+        document.write(z);
+    } else{
+        document.write(y);
+        document.write(z);
+        document.write(x);
+    }
+
+}else{ // z es el mayot
+    if(x >= y){
+        document.write(z);
+        document.write(x);
+        document.write(y); 
+    }else{
+        document.write(z);
+        document.write(y);
+        document.write(x);
+    }
+}
