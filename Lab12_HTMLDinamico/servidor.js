f3444a7bba8d10748e13ba670835d0f12da02f22
Denisse Dominguez <A01702603@itesm.mx>
2022-03-06 03:34:21 -0600

    Servidor mejorado

--- Lab12_HTMLDinamico/servidor.js
+++ Lab12_HTMLDinamico/servidor.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rutas_comidaColores = require('./routes/mod1.routes.js');
 const rutas_animalesPeliculas = require('./routes/mod2.routes');
+
 // enviar archivos html por express
 const path = require('path');
 const app = express();
@@ -13,8 +14,15 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/mod1', rutas_comidaColores);
 app.use('/mod2', rutas_animalesPeliculas);
+
+app.use((request, response, next) => {
+    response.render('inicio');
+    next();
+});
+
 
 app.use((request, response,next) => {
     response.statusCode = 404;
