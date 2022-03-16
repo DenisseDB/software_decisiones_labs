6df72f80cd31856bc8c66ce23b6c5c0f89afe571
Denisse Dominguez Bolaños <80787054+DenisseDB@users.noreply.github.com>
2022-03-16 00:21:01 -0600

    Delete Lab18_Autentificacion directory

--- Lab18_Autentificacion/util/is auth.js
+++
@@ -1,3 +0,0 @@
-// nos funciona para proteger los datos, para que necesiten hacer el login a 
-// fuerzas para poder ingresar al sitio, es decir, con modificar el / ya no se 
-// podra ingresar facilmente
